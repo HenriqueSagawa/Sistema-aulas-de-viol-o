b4f0a62d2fb84d2fe64fd03aa716658b92830c02
HenriqueSagawa <144568019+HenriqueSagawa@users.noreply.github.com>
2025-02-20 15:04:02 -0300

    Update horarios

--- ifarte/sistema-chamada/src/app/page.tsx
+++ ifarte/sistema-chamada/src/app/page.tsx
@@ -23,28 +23,28 @@ export default function Home() {
       id: "1",
       titulo: "Turma Iniciante",
       descricao: "Aulas para iniciantes em violão",
-      horario: "Quinta - 14h30",
+      horario: "Quarta: 14:30 - 15:30",
       totalAlunos: 0
     },
     {
       id: "2",
       titulo: "Turma Intermediária",
       descricao: "Aulas para alunos de nível intermediário",
-      horario: "Quinta - 15h30",
+      horario: "Quarta: 15h30 - 16:30",
       totalAlunos: 0
     },
     {
       id: "3",
       titulo: "Turma dos Idosos",
       descricao: "Aulas para alunos idosos",
-      horario: "Quinta - 13h30",
+      horario: "Quarta: 16h30 - 17h30",
       totalAlunos: 0
     },
     {
       id: "4",
       titulo: "Oficina do Música",
       descricao: "Oficinas de música do IFPR",
-      horario: "",
+      horario: "Quarta: 13:10 - 14:15",
       totalAlunos: ""
     }
   ]
